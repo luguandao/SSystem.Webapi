07772303a76b7f673526ccc7b735a572bfa9fe70
fox_sky <luguandao@sohu.com>
2015-05-13 03:06:31 +0800

    更新信息

--- implementation/src/SSystem.Webapi.Core/Properties/AssemblyInfo.cs
+++ implementation/src/SSystem.Webapi.Core/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("SSystem.Webapi.Core")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("访问http")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("webforapi")]
 [assembly: AssemblyProduct("SSystem.Webapi.Core")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
